c41648ac05abfdd1d1ee08234b0f2bc3ce4ba705
lennox charles <lennyaiko17@gmail.com>
2023-10-04 23:13:38 +0100

    fix: router bug

--- src/helpers/router.js
+++ src/helpers/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
-import HomeView from "./views/HomeView.vue"
-import NotFoundView from "./views/NotFoundView.vue"
-import AboutView from "./views/AboutView.vue"
+import HomeView from "../views/HomeView.vue"
+import NotFoundView from "../views/NotFoundView.vue"
+import AboutView from "../views/AboutView.vue"
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
