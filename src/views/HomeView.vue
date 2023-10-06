264d1d7708a85f8992d1d760cd4c91145ec03b05
lennox charles <lennyaiko17@gmail.com>
2023-10-06 15:15:52 +0100

    fix: reduce text size on phone screens

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -103,7 +103,7 @@ function scrollToTop() {
     </div>
 
     <!-- FOOTER -->
-    <div class="sticky bottom-0 items-center justify-center flex p-1 font-medium mx-auto my-0 bg-white">
+    <div class="sticky bottom-0 items-center justify-center flex p-1 font-medium mx-auto my-0 text-base xs:text-sm bg-white">
         <p>Created with 💖 by <a href="https://twitter.com/c_lennyx" target="_blank">Lennox</a> | <button @click="scrollToTop">🔝</button></p>
     </div>
 </template>
