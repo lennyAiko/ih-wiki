a96502cab3dc85b71d7b19fce6289bf7c4b9c046
lennox charles <lennyaiko17@gmail.com>
2023-10-28 14:55:56 +0100

    feat: implement sticky footer

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -10,7 +10,8 @@ function scrollToTop() {
 
 <template>
 
-    <div class="bottom-0 items-center justify-center flex p-1 mt-auto w-full h-10 text-sm xs:text-sm bg-white border-t">
+    <div class="fixed inset-x-0
+    bottom-0 items-center justify-center flex p-1 mt-auto w-full h-10 text-sm xs:text-sm bg-white border-t">
         <p>Built with 💖 by 
             <a :href="getRefLink('https://twitter.com/c_lennyx')" target="_blank">Lennox</a> | <button @click="scrollToTop">🔝</button>
         </p>
